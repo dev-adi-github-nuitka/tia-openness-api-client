d5ecb3506927bcf16c6d5a9358656b741105119d
Verve ADI <145064571+dev-adi-github-nuitka@users.noreply.github.com>
2026-02-11 11:46:33 -0600

    ✨ feat: Add support for TIA Portal versions v20 and v21

--- tia_portal/version.py
+++ tia_portal/version.py
@@ -1,11 +1,11 @@
-""" TIA Portal version. This module only has the Enum class TIAVersion. The Enum class is used to store the TIA Portal version. The TIA Portal version is used to determine the correct TIA Portal API to use. The TIA Portal version is stored in the config.ini file in the user's home directory. The config.ini file is created if it does not exist. The default version is V17. The user can change the version by calling the set_version function.
+""" TIA Portal version. This module only has the Enum class TIAVersion. The Enum class is used to store the TIA Portal version. The TIA Portal version is used to determine the correct TIA Portal API to use.
 """
 from __future__ import annotations
 from enum import Enum
 
 
 class TiaVersion(Enum):
-    """TIA Portal version. The TIA Portal version is used to determine the correct TIA Portal API to use. The TIA Portal version is stored in the config.ini file in the user's home directory. The config.ini file is created if it does not exist. The default version is V17. The user can change the version by calling the set_version function.
+    """TIA Portal version. The TIA Portal version is used to determine the correct TIA Portal API to use.
 
     Attributes:
         V15 (str): TIA Portal V15.
@@ -14,6 +14,8 @@ class TiaVersion(Enum):
         V17 (str): TIA Portal V17.
         V18 (str): TIA Portal V18.
         V19 (str): TIA Portal V19.
+        V20 (str): TIA Portal V20.
+        V21 (str): TIA Portal V21.
     """
 
     V15 = "15"
@@ -22,3 +24,5 @@ class TiaVersion(Enum):
     V17 = "17"
     V18 = "18"
     V19 = "19"
+    V20 = "20"
+    V21 = "21"
